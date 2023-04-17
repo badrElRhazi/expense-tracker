1746833f074fb7215c7d5ad788c758458efdb025
Badr El Rhazi <rzi.badr@gmail.com>
2023-04-17 13:12:59 +0000

    add 2 -NotMapped- properties

--- Expense Tracker/Models/Transaction.cs
+++ Expense Tracker/Models/Transaction.cs
@@ -9,11 +9,31 @@ namespace Expense_Tracker.Models
         [Key]
         public int TransactionId { get; set; }
         //foreign key category:
+        [Range(1,int.MaxValue,ErrorMessage ="Please select a category.")]  
         public int CategoryId { get; set; }
-        public Category Category { get; set; }
+        public Category? Category { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "Amount should be greater than 0. ")]
+
         public int Amount { get; set; }
         [Column(TypeName = "nvarchar(75)")]
         public string? Note { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
+        [NotMapped]
+        public string? CategoryTitleWithIcon
+        {
+            get
+            {
+                return Category == null ? "" : Category.Icon + " " + Category.Title;
+            }
+        }
+        [NotMapped]
+        public string? FormattedAmount
+        {
+            get
+            {
+                return ((Category == null || Category.Title=="Expense") ? "- " : "+") +Amount.ToString("c0");
+            }
+        }
+
     }
 }
